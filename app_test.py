3e4bb52f9baea1a13de2e65f2c20224b91490aae
hungtn404 <vn.hungtran2000@gmail.com>
2025-12-30 09:02:47 +0700

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -460,9 +460,8 @@ if menu == "Admin":
                 "Đường": [
                     "Điện Biên Phủ","Xô Viết Nghệ Tĩnh","Ung Văn Khiêm","Chu Văn An",
                     "Phan Đăng Lưu","Nguyễn Văn Đậu","Nguyễn Hữu Cảnh","Đinh Bộ Lĩnh",
-                    "Nguyễn Xí","Phan Xích Long","Nguyễn Gia Trí (D2)","Lê Quang Định",
+                    "Nguyễn Xí","Phan Xích Long","Nguyễn Gia Trí (D2)","Lê Quang Định","Nguyên Hồng"
                     "Phan Văn Trị","Tôn Đức Thắng","Ngô Tất Tố","Phạm Văn Đồng","Trường Sa","Đặng Thùy Trâm","Bình Lợi",
-                    "Nguyên Hồng"
                 ]
             }
             PHUNHUAN = {
@@ -1615,6 +1614,7 @@ elif menu == 'CTV':
 st.markdown("---")
 
 st.caption("App xây dựng bời hungtn AKA TRAN NGOC HUNG")
+
 
 
 
